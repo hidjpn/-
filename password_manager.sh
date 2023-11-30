cadd7a9f9faa85b3e832bc9f08db34557c26d44e
hidjpn <hidjpn@gmail.com>
2023-11-30 12:00:09 +0900

    パスワードマネージャー新規作成ステップ1まで

---
+++ password_manager.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "パスワードマネージャーへようこそ！"
+echo "サービス名を入力してください："
+read service
+echo "ユーザー名を入力してください："
+read user
+echo "パスワードを入力してください："
+read pass
+echo "Thank you !"
+echo $service $user $pass >> password_manager.txt
